c386606d63c672cc3f67984e3de8f375b530869d
Khaleed Saifullah Nayem <ksnayem@gmail.com>
2024-04-02 01:44:30 +0600

    implemented slick slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".hero-wrappper").slick({
+    dots: true,
+    arrows: true,
+    nextArrow: '<i class="fas fa-arrow-circle-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-arrow-circle-left slick-prev"></i>',
+  });
+});
